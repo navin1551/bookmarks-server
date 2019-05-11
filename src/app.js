099172aa4f24921089f5de9daec7e5e05b4b81b2
Navin Kumar <navin1551@gmail.com>
2019-05-10 22:03:40 -0700

    need to refactor into router

--- src/app.js
+++ src/app.js
@@ -29,6 +29,11 @@ app.get("/bookmarks/:bookmark_id", (req, res, next) => {
   const knexInstance = req.app.get("db");
   BookmarksService.getById(knexInstance, req.params.bookmark_id)
     .then(bookmark => {
+      if (!bookmark) {
+        return res.status(404).json({
+          error: { message: `Bookmark doesn't exist` }
+        });
+      }
       res.json(bookmark);
     })
     .catch(next);
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -20,25 +20,54 @@ describe.only("Bookmarks Endpoints", function() {
 
   afterEach("cleanup", () => db("bookmarks").truncate());
 
-  context("Given there are bookmarks in the database", () => {
-    const testBookmarks = makeBookmarksArray();
-
-    beforeEach("insert bookmarks", () => {
-      return db.into("bookmarks").insert(testBookmarks);
+  describe("GET /bookmarks", () => {
+    context("Given no bookmarks", () => {
+      it("responds with 200 and an empty list", () => {
+        return supertest(app)
+          .get("/bookmarks")
+          .expect(200, []);
+      });
     });
 
-    it("GET /bookmarks responds with 200 and all of the bookmarks", () => {
-      return supertest(app)
-        .get("/bookmarks")
-        .expect(200, testBookmarks);
+    context("Given there are bookmarks in the database", () => {
+      const testBookmarks = makeBookmarksArray();
+
+      beforeEach("insert bookmarks", () => {
+        return db.into("bookmarks").insert(testBookmarks);
+      });
+
+      it("responds with 200 and all of the bookmarks", () => {
+        return supertest(app)
+          .get("/bookmarks")
+          .expect(200, testBookmarks);
+      });
+    });
+  });
+
+  describe("GET /bookmarks/:bookmark_id", () => {
+    context("Given no bookmarks", () => {
+      it("responds with 404", () => {
+        const bookmarkId = 123456;
+        return supertest(app)
+          .get(`/bookmarks/${bookmarkId}`)
+          .expect(404, { error: { message: `Bookmark doesn't exist` } });
+      });
     });
 
-    it("GET /bookmarks/:bookmark_id responds with 200 and the specified bookmark", () => {
-      const bookmarkId = 2;
-      const expectedBookmark = testBookmarks[bookmarkId - 1];
-      return supertest(app)
-        .get(`/bookmarks/${bookmarkId}`)
-        .expect(200, expectedBookmark);
+    context("Given there are bookmarks in the database", () => {
+      const testBookmarks = makeBookmarksArray();
+
+      beforeEach("insert bookmarks", () => {
+        return db.into("bookmarks").insert(testBookmarks);
+      });
+
+      it("responds with 200 and the specified bookmark", () => {
+        const bookmarkId = 2;
+        const expectedBookmark = testBookmarks[bookmarkId - 1];
+        return supertest(app)
+          .get(`/bookmarks/${bookmarkId}`)
+          .expect(200, expectedBookmark);
+      });
     });
   });
 });
